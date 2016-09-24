af2c1e98efd49f518413ec57aafbba2a9b2ba7e6
Stephen Feather <sfeather@gmail.com>
2016-09-24 16:04:22 -0400

    add coreMotionManager variable and a function to create it if not already assigned

--- index.ios.ts
+++ index.ios.ts
@@ -4,3 +4,9 @@ declare var NSOperationQueue: any;
 interface AccelerometerData {};
 interface GyroscopeData {};
 interface MagnetometerData {};
+var coremotionManager;
+function createCoreMotionManager() {
+  if (!coremotionManager) {
+    coremotionManager = CMMotionManager.alloc().init();
+  }
+}
