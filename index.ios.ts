88f90f58c25607a862cc51a11b9e3dceeec347e3
Stephen Feather <sfeather@gmail.com>
2016-09-24 16:07:20 -0400

    add isGyroscopeAvailable() and isGyroscopeActive() methods

--- index.ios.ts
+++ index.ios.ts
@@ -7,15 +7,27 @@ interface MagnetometerData {};
 var coremotionManager;
 var accelerometerIsListening = false;
 var accelerometerUpdateInterval = 0.1;
+var gyroscopeIsListening = false;
+var gyroscopeUpdateInterval = 0.1;
 function createCoreMotionManager() {
   if (!coremotionManager) {
     coremotionManager = CMMotionManager.alloc().init();
   }
 }
+
 export function isAccelerometerAvailable(){
   createCoreMotionManager();
   return coremotionManager.accelerometerAvailable;
 }
+
+export function isGyroscopeAvailable(){
+  createCoreMotionManager();
+  return coremotionManager.gyroscopeAvailable;
+}
 export function isAccelerometerActive(){
   return accelerometerIsListening;
+}
+
+export function isGyroscopeActive(){
+  return gyroscopeIsListening;
 }
